cbb13358cdb1a80b94a82d9609da4242ac5d7d90
Prajwal Kamble <65185064+prajwalkamble@users.noreply.github.com>
2025-04-10 17:32:16 +0530

    Update dashboard.js

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -1,20 +1,19 @@
 "use server";
 
+import aj from "@/lib/arcjet";
 import { db } from "@/lib/prisma";
+import { request } from "@arcjet/next";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 const serializeTransaction = (obj) => {
   const serialized = { ...obj };
-
   if (obj.balance) {
     serialized.balance = obj.balance.toNumber();
   }
-
   if (obj.amount) {
     serialized.amount = obj.amount.toNumber();
   }
-
   return serialized;
 };
 
@@ -22,6 +21,32 @@ export async function createAccount(data) {
   try {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthoroized");
+
+    // Get request data for Arcjet
+    const req = await request();
+
+    //Check rate limit
+    const decision = await aj.protect(req, {
+      userId,
+      requested: 1, // Specify how many tokens to consume
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        const { remaining, reset } = decision.reason;
+        console.error({
+          code: "RATE_LIMIT_EXCEEDED",
+          details: {
+            remaining,
+            resetInSeconds: reset,
+          },
+        });
+
+        throw new Error("Too many requests. Please try again later.");
+      }
+
+      throw new Error("Request blocked");
+    }
 
     const user = await db.user.findUnique({
       where: { clerkUserId: userId },
@@ -42,7 +67,7 @@ export async function createAccount(data) {
       where: { userId: user.id }
     });
 
-    const shouldBeDefault = existingAccounts.length === 0? true : data.isDefault;
+    const shouldBeDefault = existingAccounts.length === 0 ? true : data.isDefault;
 
     // If the account should be default, unset other default accounts
     if (shouldBeDefault) {
@@ -82,21 +107,25 @@ export async function getUserAccounts() {
     throw new Error("User not found");
   }
 
-  const accounts = await db.account.findMany({
-    where: { userId: user.id },
-    orderBy: { createdAt: "desc" },
-    include: {
-      _count: {
-        select: {
-          transactions: true,
+  try{
+    const accounts = await db.account.findMany({
+      where: { userId: user.id },
+      orderBy: { createdAt: "desc" },
+      include: {
+        _count: {
+          select: {
+            transactions: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  const serializedAccounts = accounts.map(serializeTransaction);
+    const serializedAccounts = accounts.map(serializeTransaction);
 
-  return serializedAccounts;
+    return serializedAccounts;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 export async function getDashboardData() {
